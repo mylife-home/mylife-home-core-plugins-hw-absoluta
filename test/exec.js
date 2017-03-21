81efdf4edbd1fa94f14075f843a0dfde396478c7
Vincent Trumpff <vincent@stoic.com>
2017-03-21 18:07:38 +0100

    imap connection wip

--- test/exec.js
+++ test/exec.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const { Connection } = require('../lib/service/');
+
+const con = new Connection({
+  user     : process.argv[2],
+  password : process.argv[3],
+  host     : 'imap.gmail.com',
+  port     : 993,
+  tls      : true
+});
+
+con.on('error', (err) => console.error(err));
